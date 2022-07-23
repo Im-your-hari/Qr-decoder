09f8a6a7e20cc5f5f3d77b2d01f89b7c13f8b50e
Im-your-hari <baluoger91@gmail.com>
2022-07-23 16:03:35 +0530

    Basic GUI set

--- qrdecode.py
+++ qrdecode.py
@@ -1,6 +1,12 @@
+import os
 from pyzbar.pyzbar import decode
 from PIL import Image
+from breezypythongui import EasyFrame
+from tkinter import *
+from tkinter import filedialog
 
+#logic___________
+'''
 img = Image.open(r"qreee.jpg")
 l = decode(img)
 l=str(l[0][0])
@@ -9,3 +15,56 @@ l=str(l[0][0])
 for i in l.split(";"):
 	j = i.split(":")
 	print(j[len(j)-1])
+'''
+
+#gui____________
+class Main(EasyFrame):
+	def __init__(self):
+		EasyFrame.__init__(
+			self,
+			title="Wifi-QR-Decoder",
+			background="blue",
+			width=200,
+			height=100
+		)
+		self.wifi=self.addLabel(
+				text="Wi-Fi",
+				row=0,
+				column=0,
+				columnspan=2,
+				sticky="NSWE"
+			)
+		self.browse=self.addButton(
+				text="Browse",
+				row=1,
+				column=0,
+				command=self.read
+			)
+		self.exit=self.addButton(
+				text="Exit",
+				row=1,
+				column=1,
+				command=lambda:exit()
+			)
+		
+	def read(self):
+			fln = filedialog.askopenfilename(
+					initialdir=os.getcwd(),
+					title="Select QR Code",
+					filetypes=(
+							("JPG files","*.jpg"),
+							("PNG files","*.png"),
+							("All files","*.*")
+					)
+				)
+			print(fln)
+			self.wifi["text"]=fln			
+
+
+
+
+
+def abc():
+	Main().mainloop()
+if __name__ == "__main__":
+	abc()
